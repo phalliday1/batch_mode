7dcd6fa37b08d5cff869584dfc7f6373c7cc0024
phil <phalliday@8minutenergy.com>
2017-08-11 12:01:59 -0700

    Made user  inputs a dictionary to be returned

--- prompt.py
+++ prompt.py
@@ -7,13 +7,15 @@ Created on Thu Aug 10 15:21:07 2017
 def user_prompt():
     
     strings_ub = input("What is the maximum number of strings to consider?")
-    strings_ln = input("What is the minimum number of strings to consider?")
+    strings_lb = input("What is the minimum number of strings to consider?")
     string_its = input("How many iterations on the number of strings should be considered?")
     pitch_ub = input("What is the maximum E-W pitch to consider?")
     pitch_lb = input("What is the minimum E-W pitch to consider?")
     pitch_its = input("How many interations on pitch should be considered?")
+    
+    inputs= {'strings_ub':strings_ub, 'strings_lb':strings_lb, 'strings_its':strings_its, 'pitch_ub':pitch_ub, 'pitch_lb':pitch_lb, 'pitch_its':pitch_its}
 
-    return strings_ub, strings_ln, string_its, pitch_ub, pitch_lb, pitch_its
+    return inputs
 
 
 
